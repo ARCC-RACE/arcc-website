6e87c3c147a5976fd103298913ca87bc0d4301e3
Caelin Sutch <sutchc20@student.jhs.net>
2019-07-04 13:20:33 -0400

    Added ICO and updated menu to work correctly

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -3,6 +3,7 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 import {SlideInOutAnimation} from './animations';
 import {ActivationEnd, ActivationStart, NavigationEnd, NavigationStart, Router} from '@angular/router';
 import {skip} from 'rxjs/operators';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-nav',
@@ -10,9 +11,14 @@ import {skip} from 'rxjs/operators';
   styleUrls: ['./nav.component.scss'],
   animations: [SlideInOutAnimation]
 })
-export class NavComponent implements OnInit{
+export class NavComponent implements OnInit {
+  open = false;
+  animationState = 'out';
+  notMobile = false;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    public breakpointObserver: BreakpointObserver) {}
 
   ngOnInit() {
     // Jank solution so it doesn't open on page load
@@ -20,13 +26,25 @@ export class NavComponent implements OnInit{
       if (event instanceof NavigationEnd) {
       }
     });
+
+    this.breakpointObserver
+      .observe(['(min-width: 1200px)'])
+      .subscribe((breakpointState: BreakpointState) => {
+        if (breakpointState.matches) {
+          this.animationState = 'in';
+          this.notMobile = true;
+        } else {
+          this.animationState = 'out';
+        }
+      });
   }
-  open = false;
-  animationState = 'out';
+
 
   toggleCollapse() {
 // this.show = !this.show
-    this.animationState = this.animationState === 'out' ? 'in' : 'out';
+    if (!this.notMobile) {
+      this.animationState = this.animationState === 'out' ? 'in' : 'out';
+    }
   }
 
 }
