49da99617ceacfa3957a4f5bdc79fb81f2cec213
Caelin Sutch <sutchc20@student.jhs.net>
2019-07-08 10:52:48 -0400

    Fixed editor styles and lazy loading error

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,14 +4,12 @@ import { NavComponent } from './nav/nav.component';
 import { EmailSignupComponent } from './footer/email-signup/email-signup.component';
 import { FooterComponent } from './footer/footer.component';
 import {RouterModule} from '@angular/router';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BubblesComponent } from './bubbles/bubbles.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    BrowserAnimationsModule
   ],
   declarations: [NavComponent, EmailSignupComponent, FooterComponent, BubblesComponent],
   exports: [
