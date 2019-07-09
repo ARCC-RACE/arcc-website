4fd60e6070bacbb040778f9085d148e2d9f75042
Caelin Sutch <sutchc20@student.jhs.net>
2019-07-09 09:40:32 -0400

    Added press page

---
+++ src/app/press/press.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../_models/post.model';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-press',
+  templateUrl: './press.component.html',
+  styleUrls: ['./press.component.scss']
+})
+export class PressComponent implements OnInit {
+
+  private postsCollection: AngularFirestoreCollection<Post>;
+  posts: Observable<any>;
+  loading = true;
+
+  constructor(private afs: AngularFirestore) {
+    this.postsCollection = afs.collection<Post>('posts');
+    this.posts = this.postsCollection.snapshotChanges()
+      .pipe(map(actions => {
+        // this.loading = false;
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Post;
+          data.content = data.content.split(' ').slice(0, 20).join(' ');
+          const id = a.payload.doc.id;
+          return { id, data };
+        });
+      }));
+  }
+
+  ngOnInit() {
+  }
+
+}
